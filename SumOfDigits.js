01ff77a6cd1675c302c54a51e5879aebb0654a9b
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-17 12:29:08 +0530

    SumOfDigits:to find sumof digit step by step process

---
+++ SumOfDigits.js
@@ -0,0 +1,36 @@
+let num=78346;
+
+let remainder =num%10;
+console.log(remainder);//6
+num=Math.floor(num/10)
+console.log(num);//7834
+
+remainder =num%10;
+console.log(remainder);//4
+num=Math.floor(num/10)
+console.log(num);//783
+
+remainder =num%10;
+console.log(remainder);//3
+num=Math.floor(num/10)
+console.log(num);//78
+
+remainder =num%10;
+console.log(remainder);//8
+num=Math.floor(num/10)
+console.log(num);//7
+
+remainder =num%10;
+console.log(remainder);//7
+num=Math.floor(num/10)
+console.log(num);//0
+
+remainder =num%10;
+console.log(remainder);//0
+num=Math.floor(num/10)
+console.log(num);//0
+
+
+
+
+
