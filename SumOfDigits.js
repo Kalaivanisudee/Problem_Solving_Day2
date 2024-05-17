d7f9949c11b8b614f30fbe89d78c461830a062c3
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-17 13:22:34 +0530

    SumOfDigit:final code

--- SumOfDigits.js
+++ SumOfDigits.js
@@ -1,36 +1,14 @@
 let num=78346;
 
-let remainder =num%10;
-console.log(remainder);//6
-num=Math.floor(num/10)
-console.log(num);//7834
-
-remainder =num%10;
-console.log(remainder);//4
-num=Math.floor(num/10)
-console.log(num);//783
-
-remainder =num%10;
-console.log(remainder);//3
-num=Math.floor(num/10)
-console.log(num);//78
-
-remainder =num%10;
-console.log(remainder);//8
-num=Math.floor(num/10)
-console.log(num);//7
-
-remainder =num%10;
-console.log(remainder);//7
-num=Math.floor(num/10)
-console.log(num);//0
-
-remainder =num%10;
-console.log(remainder);//0
-num=Math.floor(num/10)
-console.log(num);//0
-
-
-
+function sumOfDigit(num){
+    let sum=0;
+while(num>0){
+    let reminder= num % 10;
+     num=Math.floor(num/10);
+    sum=sum+reminder;
+}
+return sum;
+}
+console.log(sumOfDigit(num))
 
 
