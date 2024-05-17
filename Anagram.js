6be54b1acfa48d1fb2700182f7285370fba17f60
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-17 15:34:57 +0530

    Anagram:logics

---
+++ Anagram.js
@@ -0,0 +1,12 @@
+
+// What is anagram?
+// An Anagram of a string is another string that contains the same characters, only the order of characters can be different.
+//
+let string1="they see";
+let string2="the eyes ";
+console.log(string1.length);
+console.log(string2.length);
+string1.split(" ").join("");
+string2.split(" ").join("");
+console.log(string1);
+console.log(string2);
