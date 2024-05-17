b9d488e7910eeb105bbb555c8c78c657cfda4da2
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-17 16:56:05 +0530

    Anagram :optimized code

--- Anagram.js
+++ Anagram.js
@@ -4,11 +4,40 @@
 //
 let string1="listen";
 let string2="silent";
-console.log(string1[0]);//Accessing index in string
+function isAnagram(string1,string2){
+    let myObj={}
+    for(let i=0;i<string1.length;i++){
+        if(string1[i] >= "a" && string1[i] <= "z"){//Check the character a to Z.It avoiding numbers and special character.
+            if(myObj[string1[i]]){
+                myObj[string1[i]] +=1
+            }else {
+                myObj[string1[i]]=1;
+            }
 
-let object={name:"dhamu", age:28}
-object["name"]="kalai" //Accessing keys in object 
+        }else {
+            return false;
+        }
 
-console.log(object); //{name:"kalai", age:28}
-console.log(Object.keys(object))//[ 'name', 'age' ]
+    }
+    for(let i=0;i<string2.length;i++){
+        if(string2[i] >= "a" && string2[i] <= "z"){
+            if(myObj[string2[i]]){
+                myObj[string2[i]] -=1
+                if(myObj[string2[i]]==0){
+                    delete myObj[string2[i]]
+                }
+            }else{
+                return false;
+            }
+            
+
+        }
+
+    }
+    console.log(myObj);
+ return Object.keys(myObj).length==0;
+}
+console.log(isAnagram(string1,string2))
+
+
 
