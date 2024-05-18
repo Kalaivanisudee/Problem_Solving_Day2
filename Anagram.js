253c8250a58b30946d7b6b0555b221724ac2b309
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-18 07:00:43 +0530

    Anagram : method2

--- Anagram.js
+++ Anagram.js
@@ -4,25 +4,11 @@ console.log(string1.charCodeAt(0));//108 //L ASCII
 console.log(string2.charCodeAt(0));//115 //L ASCII
 let arr=Array.from({length:256},()=>0);
 // console.log(...arr); // 256 index filled with value 0
-console.log(arr[string1.charCodeAt(0)]++); //108:1 =>Debugging output
-console.log(arr[string2.charCodeAt(0)]--); //115:-1 =>Debugging output
+if(string1.length != string2.length) return false;
+for(let i=0; i<string1.length;i++){
+    arr[string1.charCodeAt(i)]++;
+    arr[string2.charCodeAt(i)]--;
+}
+let filterArray=arr.filter((value)=>value !=0).length==0;
+console.log(filterArray);//true
 
-console.log(arr[string1.charCodeAt(1)]++); //105:1
-console.log(arr[string2.charCodeAt(1)]--);//105:-1 //0
-
-console.log(arr[string1.charCodeAt(2)]++); //115:1
-console.log(arr[string2.charCodeAt(2)]--);//108:-1
-
-console.log(arr[string1.charCodeAt(3)]++); //116:1
-console.log(arr[string2.charCodeAt(3)]--);//101:-1
-
-console.log(arr[string1.charCodeAt(4)]++); //101:1
-console.log(arr[string2.charCodeAt(4)]--);//110:-1
-
-console.log(arr[string1.charCodeAt(5)]++); //110:1
-console.log(arr[string2.charCodeAt(5)]--);//116:-1
-
-let filterArray=arr.filter((value)=>value !=0)
-console.log(filterArray);//[]
-console.log(filterArray.length);//0
-console.log(filterArray.length==0);//true
